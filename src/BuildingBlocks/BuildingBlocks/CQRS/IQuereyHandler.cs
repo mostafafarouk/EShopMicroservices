ccc4df1a334cf92b089b02eb13ce346c17fada38
mostafafarouk <mostafa.farouk@outlook.com>
2024-10-23 21:14:50 +0300

    add catalog commands and quesies build shared blocks add carter and handle endpoints for CRUD

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuereyHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<in TQuerey,TResponse>
+        :IRequestHandler<TQuerey,TResponse>
+        where TQuerey :IQuery<TResponse>
+        where TResponse:notnull
+    {
+    }
+}
