fa62cdd3103f590962f0f7be55da7a8510976880
mostafafarouk <mostafa.farouk@outlook.com>
2024-10-24 08:31:50 +0300

    add validatores

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,10 +1,22 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session)
+public class CreateProductCommandValidator:AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(r=>r.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(r => r.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(r => r.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(r => r.Price).GreaterThan(0).WithMessage("Price should be greater than 0");
+
+    }
+}
+internal class CreateProductCommandHandler(IDocumentSession session,IValidator<CreateProductCommand> validator)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -12,7 +24,12 @@ internal class CreateProductCommandHandler(IDocumentSession session)
         //create Product entity from command object
         //save to database
         //return CreateProductResult result
-
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(r => r.ErrorMessage).ToList();
+        if (errors.Any())
+        {
+            throw new ValidationException(errors.FirstOrDefault());
+        }
         var product = new Product
         {
             Name = command.Name,
